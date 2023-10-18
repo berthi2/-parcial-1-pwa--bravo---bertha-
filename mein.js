764a4a213eaf14fbb1680549b843cc7e92a96b79
bertha <bertha.bravo@davinci.edu.ar>
2023-10-17 21:42:37 -0300

    incorporacion mein

---
+++ mein.js
@@ -0,0 +1,72 @@
+
+function renderTabla() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=100`)
+        .then(response => response.json())
+        .then(data => {
+            const listado = data.results.map((elemento) => {
+                return `<tr>
+                <td>${elemento.name}</td>
+                <td>${elemento.url}</td>
+                <td>
+                <button type="button" id="${elemento.url}" class="btn btn-outline-primary btnVerMas" >
+                    Ver más
+                </button>
+                </td>
+            </tr>`
+            });
+
+            tableBody.innerHTML = listado.join('');
+
+            const botones = document.querySelectorAll('.btnVerMas');
+
+            botones.forEach((btn) => {
+                btn.addEventListener('click', () => {
+                    const urlApi = btn.id;
+
+                    fetch(urlApi)
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                            const tituloModal = document.querySelector('.modal-title');
+                            const cuerpoModal = document.querySelector('.modal-body');
+                            tituloModal.textContent = data.name;
+
+                            const tipo = data.types.map((elemento) => {
+                                return elemento.type.name;
+                            })
+                            const habilidades = data.abilities.map((elemento) => {
+                                return elemento.ability.name;
+                            })
+
+
+                            cuerpoModal.innerHTML = `
+                        <img src="${data.sprites.front_default}" class="w-25 mx-auto d-block" alt="">
+                        <p class="text-center"><strong>Peso: </strong> ${data.weight}</p>
+                        <p class="text-center"><strong>Tipo: </strong> ${tipo.join(', ')}</p>
+                        <p class="text-center"><strong>Habilidades: </strong> ${habilidades.join(', ')}</p>
+                    `;
+                            myModal.show();
+
+
+                            if (!historialPokemones.find((elemento) => elemento.name == data.name)) {
+                                historialPokemones.push({
+                                    name: data.name,
+                                    img: data.sprites.front_default
+                                })
+                            }
+
+                            console.log(historialPokemones)
+
+                            localStorage.setItem('historial', JSON.stringify(historialPokemones));
+                        })
+
+                })
+            })
+
+        })
+        .catch(error => {
+            console.error("Ha ocurrido un error:" + error)
+        })
+}
+
+renderTabla();
